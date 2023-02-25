a7a0a2b4846c95f78d70337f436fe62e309eb4e4
Fabricio Batalha <fabriciobatalha.dev@gmail.com>
2023-02-25 14:03:20 -0400

    frontend: add

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <OHeader />
+    Teste
     <router-view />
+    <OFooter />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import {} from '@/components/shared';
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    // OHeader,
+    // OFooter,
+  },
+};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
