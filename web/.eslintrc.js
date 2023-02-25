2e50b826d793bacc022cde49688844a4c4a9b7a0
Fabricio Batalha <fabriciobatalha.dev@gmail.com>
2023-02-25 11:47:07 -0400

    frontend: add eslint

---
+++ web/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/multi-word-component-names': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
+  },
+};
--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
-    </p>
+     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
